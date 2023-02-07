573aed855d9d9b4d173838869a5946ffc6115101
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2023-02-07 09:39:29 -0300

    big progress

--- src/components/ActionBox/ActionBox.jsx
+++ src/components/ActionBox/ActionBox.jsx
@@ -1,8 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
 
 export const ActionBox = () => {
-    return(
-        <div className="w-48 h-48 flex flex-col bg-red-500">
+  const [isShowing, setIsShowing] = useState(false);
+  const [color, setColor] = useState('green');
+  console.log(color)
+
+  return (
+    <div className="bg-gray-300 w-80 h-44 rounded-lg flex flex-col space-evenly py-2 px-3">
+        
+      <div className="gap-2">
+        <div className="flex justify-between">
+        <input
+          type="text"
+          placeholder="Nome..."
+          className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"
+        />
+        <div onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)} >
+        <div className={`z-50 w-4 h-4 rounded-full border-black bg-${color}-500`}>
+                    <Popover>
+                        <Transition show={isShowing}>
+                            <Popover.Panel>
+                                <div className="absolute flex flex-col mt-6 gap-1 bg-white p-2 rounded-lg">
+                                    <button className='w-4 h-4 rounded-full bg-green-500 cursor-pointer' onClick={() => setColor('green')}></button>
+                                    <button className='w-4 h-4 rounded-full bg-yellow-500 cursor-pointer' onClick={() => setColor('yellow')}></button>
+                                    <button className='w-4 h-4 rounded-full bg-red-500 cursor-pointer' onClick={() => setColor('red')}></button>
+                                </div>
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
+                </div> 
+        </div>
+        </div>
+        <div><textarea maxLength="89" typeof="text" className="w-64 h-24 resize-none p-1 bg-gray-200" placeholder="Descrição..."></textarea></div>
+      </div>
+      <div>
+        <input type="text"  
+        placeholder="Tempo (Tipo)..."
+        className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"/>
+      </div>
+    </div>
+  );
+};
+
+
+{/* <div>
+   
+        </div> */}
+
+
+{
+  /* <div className="w-48 h-48 flex flex-col bg-red-500">
             <div className="w-full h-28 flex flex-col bg-blue-600">
                 <div className='w-full h-18 flex flex-row justify-between '>
                     <div className="w-2 h-2 bg-yellow-400"></div>
@@ -15,21 +63,23 @@ export const ActionBox = () => {
             <div className="w-full h-20 ">
 
             </div>
-        </div>
-    )
+        </div> */
 }
 
-
-{/* <div className="w-full h-full flex flex-col justify-around">
+{
+  /* <div className="w-full h-full flex flex-col justify-around">
             <div className="w-screen h-33 bg-slate-400"></div>
             <div className="w-screen h-33 bg-black"></div>
             <div className="w-screen h-33 bg-slate-400"></div>
-        </div> */}
+        </div> */
+}
 
-{/* <div className='items-center'>
+{
+  /* <div className='items-center'>
             <div className='w-64 h-48 border-4 border-indigo-600'>
             <div className='w-64 h-32 border-4 border-indigo-600'>
                 <div><input placeholder='Ação' className='items-center'></input></div>
             </div>
             </div>
-        </div> */}
+        </div> */
+}
