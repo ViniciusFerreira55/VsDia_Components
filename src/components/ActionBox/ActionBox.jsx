a2cc148445194e05b2a50976236821eeefcf8204
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2023-02-09 14:56:42 -0300

    Almost Ready

--- src/components/ActionBox/ActionBox.jsx
+++ src/components/ActionBox/ActionBox.jsx
@@ -3,45 +3,78 @@ import { Popover, Transition } from "@headlessui/react";
 
 export const ActionBox = () => {
   const [isShowing, setIsShowing] = useState(false);
-  const [color, setColor] = useState('green');
-  console.log(color)
+  const [color, setColor] = useState('gray');
+
 
   return (
-    <div className="bg-gray-300 w-80 h-44 rounded-lg flex flex-col space-evenly py-2 px-3">
-        
-      <div className="gap-2">
-        <div className="flex justify-between">
-        <input
-          type="text"
-          placeholder="Nome..."
-          className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"
-        />
-        <div onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)} >
-        <div className={`z-50 w-4 h-4 rounded-full border-black bg-${color}-500`}>
-                    <Popover>
-                        <Transition show={isShowing}>
-                            <Popover.Panel>
-                                <div className="absolute flex flex-col mt-6 gap-1 bg-white p-2 rounded-lg">
-                                    <button className='w-4 h-4 rounded-full bg-green-500 cursor-pointer' onClick={() => setColor('green')}></button>
-                                    <button className='w-4 h-4 rounded-full bg-yellow-500 cursor-pointer' onClick={() => setColor('yellow')}></button>
-                                    <button className='w-4 h-4 rounded-full bg-red-500 cursor-pointer' onClick={() => setColor('red')}></button>
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </Popover>
-                </div> 
+    <div className="w-80 h-48 bg-gray-300 rounded-lg">
+        <div className="p-2 border-b border-black flex flex-row justify-between"> 
+          <input type="text" maxLength={29} placeholder="Nome" className="bg-gray-300 w-60 placeholder-black outline-none" />
+          <div onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)} > 
+          <div className={`z-50 w-4 h-4 rounded-tr-lg border-black bg-${color}-500`}>
+          <Popover>
+            <Transition show={isShowing}>
+              <Popover.Panel>
+                <div className="absolute flex flex-col mt-6 gap-1 bg-white p-2 rounded-lg">
+                  <button className='w-4 h-4 rounded-full bg-green-500 cursor-pointer' onClick={() => setColor('green')}></button>
+                  <button className='w-4 h-4 rounded-full bg-yellow-500 cursor-pointer' onClick={() => setColor('yellow')}></button>
+                  <button className='w-4 h-4 rounded-full bg-red-500 cursor-pointer' onClick={() => setColor('red')}></button>
+                  <button className='w-4 h-4 rounded-full bg-gray-500 hidden'></button>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+          </div>
+          </div>
         </div>
+        <div className="p-2">
+          <textarea placeholder="Descrição..." maxLength={154} className="bg-gray-300 w-72 h-24 placeholder-black outline-none resize-none"/>
         </div>
-        <div><textarea maxLength="89" typeof="text" className="w-64 h-24 resize-none p-1 bg-gray-200" placeholder="Descrição..."></textarea></div>
-      </div>
-      <div>
-        <input type="text"  
-        placeholder="Tempo (Tipo)..."
-        className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"/>
-      </div>
+        <div className="flex flex-row pl-2 justify-between">
+          <div className="">
+            Numero
+          </div>
+          <div>
+            <input type="text" placeholder="Tempo" maxLength={10} className="bg-gray-300 right-0 placeholder-black w-24 h-6 outline-none"/>
+          </div>
+        </div>
     </div>
   );
 };
+
+{/* <div className="bg-gray-300 w-80 h-44 rounded-lg flex flex-col space-evenly py-2 px-3">
+        
+        <div className="gap-2">
+          <div className="flex justify-between">
+          <input
+            type="text"
+            placeholder="Nome..."
+            className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"
+          />
+          <div onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)} >
+          <div className={`z-50 w-4 h-4 rounded-full border-black bg-${color}-500`}>
+                      <Popover>
+                          <Transition show={isShowing}>
+                              <Popover.Panel>
+                                  <div className="absolute flex flex-col mt-6 gap-1 bg-white p-2 rounded-lg">
+                                      <button className='w-4 h-4 rounded-full bg-green-500 cursor-pointer' onClick={() => setColor('green')}></button>
+                                      <button className='w-4 h-4 rounded-full bg-yellow-500 cursor-pointer' onClick={() => setColor('yellow')}></button>
+                                      <button className='w-4 h-4 rounded-full bg-red-500 cursor-pointer' onClick={() => setColor('red')}></button>
+                                  </div>
+                              </Popover.Panel>
+                          </Transition>
+                      </Popover>
+                  </div> 
+          </div>
+          </div>
+          <div><textarea maxLength="89" typeof="text" className="w-64 h-24 resize-none p-1 bg-gray-200" placeholder="Descrição..."></textarea></div>
+        </div>
+        <div>
+          <input type="text"  
+          placeholder="Tempo (Tipo)..."
+          className="bg-gray-300 placeholder:text-gray-600 p-1  text-black focus:outline-none"/>
+        </div>
+      </div> */}
 
 
 {/* <div>
